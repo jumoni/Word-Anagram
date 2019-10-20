408b5e14f774dac9271e5c791574af047223214c
jumoni <liwenf@andrew.cmu.edu>
2019-10-20 11:36:29 -0400

    no single letter and subset input

--- Main.java
+++ Main.java
@@ -1,111 +1,154 @@
 import java.io.*;
-import java.util.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
-import java.nio.file.*; 
+import java.nio.file.*;
+import java.util.*;
 
+/**
+ * Main class executes some tests.
+ *
+ * @author liwen feng
+ * @version 2.0
+ */
 class Main {
-  public static void main(String[] args) {
-    Dict dict = new Dict();
-    
-    System.out.println("************run tests************");
-    try(BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
-        String content = new String(Files.readAllBytes(Paths.get("test.txt"))); 
-        String[] inputs = content.split("\n");
-        for (String input : inputs) {
-          System.out.println("input: "+input);
-          System.out.println("output: "+dict.getAnagram(input));
-          System.out.println(" ");
+    public static void main(String[] args) {
+        Dict dict = new Dict();
+
+        System.out.println("************run tests************");
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String content = new String(Files.readAllBytes(Paths.get("test.txt")));
+            String[] inputs = content.split("\n");
+            for (String input : inputs) {
+                System.out.println("input: " + input);
+                System.out.println("output: " + dict.getAnagram(input));
+                System.out.println(" ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
+        System.out.println("************end tests************");
     }
-    System.out.println("************end tests************");
-  }
 }
 
+/**
+ * This class creates a dictionary of words and
+ * implements searching for the word anagrams.
+ */
 class Dict {
-  final String src = "https://raw.githubusercontent.com/lad/words/master/words";
-  String[] dict;
-  HashMap<String, List<String>> map;
-  
-  public Dict() {
-    buildDict();
-    buildMap();
-  }
-  
-  private void buildMap() {
-    map = new HashMap<>();
-    
-    for (String word : dict) {
-      int[] cnt = new int[26];
-      for (char c : word.toCharArray()) {
-        // 'Jean-Christophe'
-        if (Character.toLowerCase(c) - 'a' < 0 ||
-            Character.toLowerCase(c) - 'a' > 25)
-          continue;
-        cnt[Character.toLowerCase(c) - 'a']++;
-      }
-      
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0;i < 26;i++) {
-        sb.append("#");
-        sb.append(cnt[i]);
-      }
-      String key = sb.toString();
-      
-      List<String> list = map.getOrDefault(key, new ArrayList<>());
-      list.add(word);
-      map.put(key, list);
+    /**
+     * The link for downloading all the words.
+     */
+    final String src = "https://raw.githubusercontent.com/lad/words/master/words";
+    /**
+     * An array of all the words.
+     */
+    String[] dict;
+    /**
+     * A map built from the dictionary
+     * key: hashcode using the lowercase letter frequencies
+     * value: a list of words
+     */
+    HashMap<String, List<String>> map;
+
+    public Dict() {
+        buildDict();
+        buildMap();
     }
-  }
-  
-  public List<String> getAnagram(String target) {
-    int[] cnt = new int[26];
-    for (char c : target.toCharArray()) {
-      cnt[Character.toLowerCase(c) - 'a']++;
-    }
-    
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0;i < 26;i++) {
-      sb.append("#");
-      sb.append(cnt[i]);
-    }
-    String key = sb.toString();
-    
-    if (map.containsKey(key)) return map.get(key);
-    else return new ArrayList<>();
-  }
-  
-  private void buildDict() {
-    StringBuilder sb = new StringBuilder();
-    URLConnection urlConn = null;
-    InputStreamReader in = null;
-    try {
-      URL url = new URL(src);
-      urlConn = url.openConnection();
-      if (urlConn != null)
-        urlConn.setReadTimeout(60 * 1000);
-      if (urlConn != null && urlConn.getInputStream() != null) {
-        in = new InputStreamReader(urlConn.getInputStream(),
-            Charset.defaultCharset());
-        BufferedReader bufferedReader = new BufferedReader(in);
-        if (bufferedReader != null) {
-          int cp;
-          while ((cp = bufferedReader.read()) != -1) {
-            sb.append((char) cp);
-          }
-          bufferedReader.close();
+
+    /**
+     * Builds the hashmap
+     */
+    private void buildMap() {
+        map = new HashMap<>();
+
+        for (String word : dict) {
+            int[] cnt = new int[26];
+            for (char c : word.toCharArray()) {
+                // deal with 'Jean-Christophe'
+                if (Character.toLowerCase(c) - 'a' < 0 ||
+                        Character.toLowerCase(c) - 'a' > 25)
+                    continue;
+                cnt[Character.toLowerCase(c) - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(cnt[i]);
+            }
+            String key = sb.toString();
+
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            // skip one letter word
+            if (word.length() > 1)
+                list.add(word);
+            map.put(key, list);
         }
-      }
-    in.close();
-    } catch (Exception e) {
-      throw new RuntimeException("Exception while calling URL:"+ src, e);
-    } 
- 
-    String all = sb.toString();
-    dict = all.split("\n");
-  }
-  
+    }
+
+    /**
+     * Builds the dict
+     */
+    private void buildDict() {
+        StringBuilder sb = new StringBuilder();
+        URLConnection urlConn = null;
+        InputStreamReader in = null;
+        try {
+            URL url = new URL(src);
+            urlConn = url.openConnection();
+            if (urlConn != null)
+                urlConn.setReadTimeout(60 * 1000);
+            if (urlConn != null && urlConn.getInputStream() != null) {
+                in = new InputStreamReader(urlConn.getInputStream(),
+                        Charset.defaultCharset());
+                BufferedReader bufferedReader = new BufferedReader(in);
+                if (bufferedReader != null) {
+                    int cp;
+                    while ((cp = bufferedReader.read()) != -1) {
+                        sb.append((char) cp);
+                    }
+                    bufferedReader.close();
+                }
+            }
+            in.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception while calling URL:" + src, e);
+        }
+
+        String all = sb.toString();
+        dict = all.split("\n");
+    }
+
+    /**
+     *
+     * Gets all possible word anagrams given the input set.
+     * @param target the input characters set
+     * @return a list of all possible English words from the dictionary
+     *         that could be composed of these characters
+     */
+    public List<String> getAnagram(String target) {
+        Set<String> res = new HashSet<>();
+
+        for (int i = 0; i < target.length(); i++) {
+            for (int j = i + 1; j <= target.length(); j++) {
+                String tmp = target.substring(i, j);
+                int[] cnt = new int[26];
+                for (char c : tmp.toCharArray()) {
+                    cnt[Character.toLowerCase(c) - 'a']++;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < 26; k++) {
+                    sb.append("#");
+                    sb.append(cnt[k]);
+                }
+                String key = sb.toString();
+
+                if (map.containsKey(key)) res.addAll(map.get(key));
+            }
+        }
+
+        return new ArrayList<>(res);
+    }
+
 }
